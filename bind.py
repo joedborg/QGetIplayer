daf69be2e6f6eb20981c8d84166c5211750c9b29
Joe Borg <cyborg101010@gmail.com>
2014-02-28 20:13:52 +0000

    Adding Programme class

--- bind.py
+++ bind.py
@@ -1,5 +1,15 @@
 import subprocess
 
+
+class Programme(object):
+    """
+    Holds all the attributes of a programme.
+    """
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__setattr__(key, value)
+
+
 class GetIplayerBind(object):
     """
     Bind to the get_iplayer Perl script.
@@ -14,3 +24,7 @@ class GetIplayerBind(object):
         """
         Return all the programmes.
         """
+        programmes = []
+        output = subprocess.check_output([self.executable])
+        for line in output:
+            programmes.append(Programme())
