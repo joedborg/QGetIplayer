6f2388aae1700298f362785d981900bb37f61065
Joe Borg <cyborg101010@gmail.com>
2014-03-01 18:52:17 +0000

    Moving get*Programmes methods into one

--- bind.py
+++ bind.py
@@ -20,11 +20,31 @@ class GetIplayerBind(object):
         """
         self.executable = "/home/joe/Projects/get_iplayer/get_iplayer"
 
-    def getAllProgrammes(self):
+    def getProgrammes(self, keywords=[], categories=[], channels=[], exclude_keywords=[], exclude_categories=[], exclude_channels=[]):
         """
-        Return all the programmes.
+        Return a filtered list of programmes.
         """
         programmes = []
-        output = subprocess.check_output([self.executable])
+        execute = [self.executable]
+        if keywords:
+            execute.append(','.join(keywords))
+        if catergories:
+            execute.append('--catergories')
+            execute.append(','.join(catergories))
+        if channels:
+            execute.append('--channels')
+            execute.append(','.join(channels))
+        if exclude_keywords:
+            execute.append('--exclude')
+            execute.append(','.join(exclude_keywords))
+        if exclude_catergories:
+            execute.append('--exclude-catergory')
+            execute.append(','.join(exclude_catergories))
+        if exclude_channel:
+            execute.append('--exclude-channel')
+            execute.append(','.join(execlude_channel))
+
+        proc = subprocess.Popen(execute, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = proc.read()
         for line in output:
-            programmes.append(Programme())
+            programmes.append(Programme(line.split()))
